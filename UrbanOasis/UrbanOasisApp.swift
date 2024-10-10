9af9167facc26c06d691093ed1074f124057e0da
uclalch <uclalch@ucla.edu>
2024-10-10 12:53:05 +0800

    Initial Commit

---
+++ UrbanOasis/UrbanOasisApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanOasisApp.swift
+//  UrbanOasis
+//
+//  Created by Larry LI on 2024/10/10.
+//
+
+import SwiftUI
+
+@main
+struct UrbanOasisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
